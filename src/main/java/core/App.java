aa840f232b58b8c783ac6f8f5a4c0873a1324c8a
246_135 <My_email_adress@gmail.com>
2017-05-11 14:18:38 -0700

    Changed anagram

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -4,8 +4,8 @@ public class App {
 
        public static void main(String[] args) {
 
-    System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
-    //System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
+    //System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+    System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
        }
 
 }
