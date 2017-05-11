350f0a12c531b620a9570065ad303e8b4c25ec9b
246_135 <My_email_adress@gmail.com>
2017-05-11 13:57:47 -0700

    Initial commit

---
+++ src/main/java/core/App.java
@@ -0,0 +1,11 @@
+package core;
+ 
+public class App {
+
+       public static void main(String[] args) {
+
+    System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+    //System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
+       }
+
+}
